89575ac012e4ffac715951412e7c5e91b5e09198
soidid <soidid@gmail.com>
2016-06-08 13:58:53 +0800

    ipv4

--- server.es6.js
+++ server.es6.js
@@ -1,17 +1,16 @@
 import express from 'express'
 let app = express()
 let port = process.env.PORT || 3000;
+
 app.get('*',(req, res)=>{
 	res.json({
 		ipaddress:req.ip,
 		language: req.headers['accept-language'].split(',')[0],
 		software: req.headers['user-agent'].split(')')[0].split('(')[1]
 	})
-	
-	console.log(req.headers);
 	res.end()
 })
-app.listen(port,(err)=>{
+app.listen(port,'127.0.0.1', (err)=>{
 	if(err) {
 		console.log('Error!'+err)
 		return;
--- server.js
+++ server.js
@@ -8,17 +8,16 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 let app = (0, _express2.default)();
 let port = process.env.PORT || 3000;
+
 app.get('*', function (req, res) {
 	res.json({
 		ipaddress: req.ip,
 		language: req.headers['accept-language'].split(',')[0],
 		software: req.headers['user-agent'].split(')')[0].split('(')[1]
 	});
-
-	console.log(req.headers);
 	res.end();
 });
-app.listen(port, function (err) {
+app.listen(port, '127.0.0.1', function (err) {
 	if (err) {
 		console.log('Error!' + err);
 		return;
