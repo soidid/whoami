dc859c80aa5dd6eec044a437ef581a43dfbcd735
soidid <soidid@gmail.com>
2016-06-08 14:06:11 +0800

    ipv4

--- server.es6.js
+++ server.es6.js
@@ -5,8 +5,7 @@ let port = process.env.PORT || 3000;
 app.get('*',(req, res)=>{
 	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 	res.json({
-		ipaddress:req.ip.split('::ffff:')[1],
-		ip: ip,
+		ipaddress:ip,
 		language: req.headers['accept-language'].split(',')[0],
 		software: req.headers['user-agent'].split(')')[0].split('(')[1]
 	})
