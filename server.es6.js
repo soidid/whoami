bff19875dda80a94275c47f7c39e407289b10840
soidid <soidid@gmail.com>
2016-06-08 14:05:16 +0800

    test ipv4

--- server.es6.js
+++ server.es6.js
@@ -3,8 +3,10 @@ let app = express()
 let port = process.env.PORT || 3000;
 
 app.get('*',(req, res)=>{
+	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 	res.json({
 		ipaddress:req.ip.split('::ffff:')[1],
+		ip: ip,
 		language: req.headers['accept-language'].split(',')[0],
 		software: req.headers['user-agent'].split(')')[0].split('(')[1]
 	})
--- server.js
+++ server.js
@@ -10,8 +10,10 @@ let app = (0, _express2.default)();
 let port = process.env.PORT || 3000;
 
 app.get('*', function (req, res) {
+	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 	res.json({
 		ipaddress: req.ip.split('::ffff:')[1],
+		ip: ip,
 		language: req.headers['accept-language'].split(',')[0],
 		software: req.headers['user-agent'].split(')')[0].split('(')[1]
 	});
